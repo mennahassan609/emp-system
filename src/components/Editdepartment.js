1834d936428ea19b752314fec099ed7f7058ad8c
mennahassan609 <68860146+mennahassan609@users.noreply.github.com>
2024-12-12 17:01:06 +0200

    Update Editdepartment.js

--- src/components/Editdepartment.js
+++ src/components/Editdepartment.js
@@ -3,15 +3,20 @@ import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
 
 const EditDepartment = () => {
     const [department, setDepartment] = useState({
         DepartmentName: '',
         NumberofEmployees: 0,
+        Company: '', 
     });
+    const [companies, setCompanies] = useState([]);
     const { id } = useParams();
     const navigate = useNavigate();
-
     useEffect(() => {
         axios.get(`http://127.0.0.1:8000/api/department_api/${id}/`)
             .then((response) => {
@@ -22,14 +27,25 @@ const EditDepartment = () => {
             });
     }, [id]);
 
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/api/company_api/')
+            .then((response) => {
+                setCompanies(response.data);
+            })
+            .catch((error) => {
+                console.error('There was an error fetching the companies!', error);
+            });
+    }, []);
+
     const handleSubmit = (event) => {
         event.preventDefault();
+        console.log('Department Data:', department);
         axios.put(`http://127.0.0.1:8000/api/department_api/${id}/`, department)
             .then(() => {
                 navigate('/departments');
             })
             .catch((error) => {
-                console.error('There was an error updating the department!', error);
+                console.error('There was an error updating the department!', error.response);
             });
     };
 
@@ -51,6 +67,7 @@ const EditDepartment = () => {
                     value={department.DepartmentName}
                     onChange={handleChange}
                     required
+                    fullWidth
                 />
                 <TextField
                     label="Number of Employees"
@@ -59,8 +76,28 @@ const EditDepartment = () => {
                     value={department.NumberofEmployees}
                     onChange={handleChange}
                     required
+                    fullWidth
                 />
-                <Button type="submit">Update Department</Button>
+
+                <FormControl fullWidth required>
+                    <InputLabel>Company</InputLabel>
+                    <Select
+                        name="Company"
+                        value={department.Company}
+                        onChange={handleChange}
+                        label="Company"
+                    >
+                        {companies.map((company) => (
+                            <MenuItem key={company.id} value={company.id}>
+                                {company.CompanyName}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+
+                <Button type="submit" variant="contained" sx={{ marginTop: 2 }}>
+                    Update Department
+                </Button>
             </form>
         </div>
     );
